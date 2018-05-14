7b5822fbde60f77586ff0e6982094e91100a461d
Miguelo0098 <i62rarum@uco.es>
2018-05-14 17:25:55 +0200

    Added Vertice y Grafo

---
+++ Practica-4-Arbol-Abarcador/grafo.hpp
@@ -0,0 +1,14 @@
+/*!
+  \file   grafo.hpp
+  \brief  Fichero de cabecera de la clase Grafo
+  \author Miguel Ángel Ramírez Ruiz
+  \date   14/05/2018
+*/
+
+#ifndef GRAFO_HPP
+#define GRAFO_HPP
+
+
+
+
+#endif
---
+++ Practica-4-Arbol-Abarcador/vertice.hpp
@@ -0,0 +1,89 @@
+/*!
+  \file   vertice.hpp
+  \brief  Fichero de cabecera de la clase Vertice
+  \author Miguel Ángel Ramírez Ruiz
+  \date   14/05/2018
+*/
+
+#ifndef VERTICE_HPP
+#define VERTICE_HPP
+
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <string>
+
+define COTA_ERROR 1.0e-6
+
+using std::istream;
+using std::ostream;
+
+namespace ed{
+
+	class Vertice{
+	private:
+		double _x;
+		double _y;
+		int _label
+
+
+	public:
+		inline Vertice(double x=0.0, double y=0.0, int label=-1){
+			setX(x);
+			setY(y);
+			setLabel(label);
+			#ifndef NDEBUG
+          		assert(std::abs(getX() - x) <= COTA_ERROR);
+          		assert(std::abs(getY() - y) <= COTA_ERROR);
+          		assert(getLabel() == label);
+        	#endif //NDEBUG
+		}
+
+		inline Vertice(Vertice const &v){
+			setX(v.getX());
+			setY(v.getY());
+			setLabel(v.getLabel());
+			#ifndef NDEBUG
+          		assert(std::abs(getX() - v.getX()) <= COTA_ERROR);
+          		assert(std::abs(getY() - v.getY()) <= COTA_ERROR);
+          		assert(getLabel() == v.getLabel());
+        	#endif //NDEBUG
+		}
+
+		inline double getX(){
+			return _x;
+		}
+
+		inline double getY(){
+			return _y;
+		}
+
+		double getLabel();
+
+		inline void setX(double x){
+			_x = x;
+
+			#ifndef NDEBUG
+          		assert(std::abs(getX() - x) <= COTA_ERROR);
+        	#endif //NDEBUG
+		}
+
+		inline void setY(double y){
+			_y = y;
+
+			#ifndef NDEBUG
+          		assert(std::abs(getY() - y) <= COTA_ERROR);
+        	#endif //NDEBUG
+		}
+
+
+
+
+		
+		
+	};
+
+
+}
+
+#endif
