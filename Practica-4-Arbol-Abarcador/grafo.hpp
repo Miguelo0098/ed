3845ea2df2968bc8ff0ba550ef5f7b862219cc93
Miguelo0098 <i62rarum@uco.es>
2018-05-28 14:14:45 +0200

    Añadidas varias funciones de la clase grafo

--- Practica-4-Arbol-Abarcador/grafo.hpp
+++ Practica-4-Arbol-Abarcador/grafo.hpp
@@ -35,6 +35,7 @@ namespace ed{
 			{
 				for (int j = 0; j < getnVertices(); ++j)
 				{
+
 					_matrizAdyacencia[(i * getnVertices()) + j] = getDistanciaEuclidea(_vertices[i], _vertices[j]);
 				}
 			}
@@ -42,11 +43,79 @@ namespace ed{
 
 
 	public:
-		Grafo();
+		
+
+		inline Grafo(){
+			#ifndef NDEBUG
+				// Se comprueba la postcondicion
+				assert(isEmpty() == true);
+			#endif //NDEBUG
+		}
 		
 		inline int getnVertices(){
 			return _vertices.size();
 		}
+
+		inline bool isEmpty(){
+			if (_vertices.empty() == true)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		inline bool hasVertice(ed::Vertice & v){
+			for (std::vector<ed::Vertice>::iterator i = _vertices.begin(); i != _vertices.end(); ++i)
+			{
+				if (*i == v)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		inline ed::Vertice & getVertice(int i){
+			#ifndef NDEBUG
+				// Se comprueba la precondicion
+				assert(isEmpty() == false);
+				assert(i >= 0);
+				assert(i < _vertices.size());
+			#endif //NDEBUG
+
+			return _vertices[i];
+		}
+
+		ed::Grafo & getPrimTree();
+
+		ed::Grafo & getKruskalTree();
+
+		//Modificadores
+
+		inline void addVertex(ed::Vertice & vertice){
+			_justAddVertice(vertice);
+			_remakeMatrix();
+			#ifndef NDEBUG
+				// Se comprueba la postcondicion
+				assert(isEmpty() == false);
+			#endif //NDEBUG
+		}
+
+		inline void removeVertex(int i){
+			#ifndef NDEBUG
+				// Se comprueba la precondicion
+				assert(isEmpty() == false);
+				assert(i >= 0);
+				assert(i < _vertices.size());
+			#endif //NDEBUG
+
+			_vertices.erase(_vertices.begin() + i);
+			_remakeMatrix;
+		}
+
+		void escribirGrafo();
+
 		
 	};
 }
